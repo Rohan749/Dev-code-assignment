f27b0aae7da5e48b96f8795e3eb312d9a313d8fe
aaruni <rohanpandey749@gmail.com>
2023-04-07 08:14:55 +0530

    Updates

---
+++ precache-manifest.40202297676370263bb3514f83c9d773.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "611e5ff0909e9929fe3d",
+    "url": "/DevCode/static/css/main.d99a3208.chunk.css"
+  },
+  {
+    "revision": "611e5ff0909e9929fe3d",
+    "url": "/DevCode/static/js/main.611e5ff0.chunk.js"
+  },
+  {
+    "revision": "d5441b9d60c38cc885b2",
+    "url": "/DevCode/static/css/1.9c6be952.chunk.css"
+  },
+  {
+    "revision": "d5441b9d60c38cc885b2",
+    "url": "/DevCode/static/js/1.d5441b9d.chunk.js"
+  },
+  {
+    "revision": "49870a368206bf157641",
+    "url": "/DevCode/static/js/runtime~main.49870a36.js"
+  },
+  {
+    "revision": "c93d54709c0d42dd082fd65c7fda1092",
+    "url": "/DevCode/static/media/devs.c93d5470.avif"
+  },
+  {
+    "revision": "cb5984f13d96b8c2900f5b23ae677db8",
+    "url": "/DevCode/index.html"
+  }
+];
